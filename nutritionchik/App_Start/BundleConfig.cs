ec2ac2bfdbb8c6a474b522d2135fa058d06619c1
sean_r_moran@msn.com <sean_r_moran@msn.com>
2014-11-08 17:25:21 -0500

    links at top of page now have sub-menus

--- nutritionchik/App_Start/BundleConfig.cs
+++ nutritionchik/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace nutritionchik
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/Site").Include(
+                        "~/Scripts/Site.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ nutritionchik/Scripts/Site.js
@@ -0,0 +1,6 @@
+﻿$("nav").on("mouseenter", ".dropDown a", function () {
+    $(this).siblings(".dropDown").toggle();
+});
+$("nav").on("mouseleave", ".dropDown .dropDown", function () {
+    $(this).toggle();
+});
